353d72489abada5b2628f54e8663e04c8923a3ea
User <tamerlankha@code.edu.az>
2022-06-03 20:02:11 +0400

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Tamerlan";
+            int Age = 30;
+            AllData(name);
+            AllData(Age);
+            
+        }
+        static void AllData(object a) 
+        {
+            Console.WriteLine(a);
+        
+        }
+    }
+}
